2f4541c0750e0abcaf924fbb01d838426dcf2e4a
Khairul Hidayat <contact@khairul.my.id>
2019-07-22 03:10:28 +0700

    Testing unit test with jest

--- App.js
+++ App.js
@@ -1,12 +1,24 @@
-import React, { Component } from "react";
-import { Text, View } from "react-native";
+import React, { Component } from 'react';
+import { Text, View, StyleSheet } from 'react-native';
+import Header from './sources/Components/Header';
+import Button from './sources/Components/Button';
 
 export default class App extends Component {
 	render() {
 		return (
-			<View>
-				<Text>App</Text>
+			<View style={{ flex: 1 }}>
+				<Header title="Test" />
+				<View style={styles.container}>
+					<Text>App</Text>
+					<Button full title="Test" />
+				</View>
 			</View>
 		);
 	}
 }
+
+const styles = StyleSheet.create({
+	container: {
+		padding: 16
+	}
+});
