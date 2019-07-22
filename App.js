70677b417d876a9dcd558e33b6766e1abbe9cbfb
Khairul Hidayat <contact@khairul.my.id>
2019-07-22 12:03:35 +0700

    Installed prettier config for eslint

--- App.js
+++ App.js
@@ -1,24 +1,27 @@
-import React, { Component } from 'react';
-import { Text, View, StyleSheet } from 'react-native';
-import Header from './sources/Components/Header';
-import Button from './sources/Components/Button';
+import React, { Component } from "react";
+import { Text, View, StyleSheet } from "react-native";
+import Header from "./sources/Components/Header";
+import Button from "./sources/Components/Button";
 
 export default class App extends Component {
-	render() {
-		return (
-			<View style={{ flex: 1 }}>
-				<Header title="Test" />
-				<View style={styles.container}>
-					<Text>App</Text>
-					<Button full title="Test" />
-				</View>
-			</View>
-		);
-	}
+  render() {
+    return (
+      <View style={styles.container}>
+        <Header title="Test" />
+        <View style={styles.container}>
+          <Text>App</Text>
+          <Button full title="Test" />
+        </View>
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
-	container: {
-		padding: 16
-	}
+  container: {
+    flex: 1
+  },
+  content: {
+    padding: 16
+  }
 });
